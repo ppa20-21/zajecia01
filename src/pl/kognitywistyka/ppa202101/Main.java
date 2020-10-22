74fae80a003b0326af78eaeff44aeb05cef38210
Piotr Wilkin <piotr.wilkin@syndatis.com>
2020-10-22 17:21:47 +0200

    Dodaliśmy autora

--- src/pl/kognitywistyka/ppa202101/Main.java
+++ src/pl/kognitywistyka/ppa202101/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
+/**
+ * @author Piotr Wilkin
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
